89c4696fe4bd0e59a39e657840ad8f06f89b82b6
Shreshtha-Kumar <shreshtha6300@gmail.com>
2025-01-11 18:37:16 +0530

    First Local file commit

---
+++ Cp1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+//Codechef problem 16
+int check(char *st,int i,int *pair){
+    int c;
+    for(i;st[i]!='\0';i+=2){
+        if(st[i+2]=='>'){
+            *pair+=1;
+            c=i;
+        }
+        else break;
+    }
+    return c;
+}
+
+int main(){
+    int t;
+    scanf("%d",&t);
+    while(t--){
+        int pair=0;
+        char st[100001];
+        scanf("%s",st);
+        for(int i=0;i<strlen(st);i++){
+            if(st[i]=='>'){
+                i=check(st,i,&pair);
+            }
+        }
+        printf("%d\n",pair);
+    }
+    return 0;
+}
---
+++ ReadCP1.md
@@ -0,0 +1,4 @@
+# Hello, this code gives the result for Problem 16 of codechef
+The main logic behind is to count the number '>',<br>
+The function counts the occurences of '>',after one is encountered in <br>
+the main function and the function return the next index to continue from.<br>Skipping the already done cases
